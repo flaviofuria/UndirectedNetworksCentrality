bea8ee3483acc2a397ad87d9d46427ee17d188c1
Flavio Furia <furia.flavio@outlook.it>
2021-01-24 18:42:09 +0100

    first commit

---
+++ centralities.py
@@ -0,0 +1,92 @@
+import networkx as nx
+import numpy as np
+
+def degree(g: nx.Graph()) -> dict:
+    scores = {}
+    for k, v in g.adj.items():
+        scores[k] = len(v)
+    return scores
+
+
+def closeness(g: nx.Graph()) -> dict:
+    scores = {}
+    for x in g.nodes:
+        sum_of_distance_from_x = 0
+        for y in g.nodes:
+            if x != y and nx.has_path(g, x, y):
+                sum_of_distance_from_x += nx.shortest_path_length(g, x, y)
+        if sum_of_distance_from_x == 0:
+            scores[x] = 0
+        else:
+            scores[x] = 1/sum_of_distance_from_x
+    return scores
+
+
+def lin(g: nx.Graph()) -> dict:
+    scores = {}
+    for x in g.nodes:
+        sum_of_distance_from_x = 0
+        reachable_from_x = 0
+        for y in g.nodes:
+            if x != y and nx.has_path(g, x, y):
+                sum_of_distance_from_x += nx.shortest_path_length(g, x, y)
+                reachable_from_x += 1
+        if reachable_from_x == 0:
+            scores[x] = 1
+        else:
+            scores[x] = (reachable_from_x ** 2)/sum_of_distance_from_x
+    return scores
+
+
+def harmonic(g: nx.Graph()) -> dict:
+    return nx.harmonic_centrality(g)
+
+
+def eigenvector(g: nx.Graph()) -> dict:
+    return {k: round(v, 5) for k, v in nx.eigenvector_centrality(g).items()}
+
+
+def seeley(g: nx.Graph()) -> dict:
+    scores = {}
+    adj_mat = np.array([[0] * len(g.nodes)] * len(g.nodes), dtype=float)
+    for k, v in nx.convert.to_dict_of_lists(g).items():
+        for pos in v:
+            adj_mat[k - 1][pos - 1] = 1
+
+    for i in range(len(g.nodes)):
+        row_sum = np.sum(adj_mat[i])
+        if row_sum > 0:
+            adj_mat[i] = np.divide(adj_mat[i], row_sum)
+
+    eigenvalues, eigenvectors = np.linalg.eig(adj_mat)
+    dom_eigenvalue = max(np.amax(eigenvalues), np.absolute(np.amin(eigenvalues)))
+    max_eigenvalue_index = np.argmax(np.absolute(eigenvalues))
+    dom_eigenvector = np.vstack(eigenvectors[:, max_eigenvalue_index]).flatten()
+
+    for i in range(len(dom_eigenvector)):
+        scores[i+1] = dom_eigenvector[i]
+    return scores
+
+
+def katz(g: nx.Graph()) -> dict:
+    pass
+
+
+def pagerank(g: nx.Graph()) -> dict:
+    pass
+
+
+if __name__ == '__main__':
+    edges = [(1, 2), (1, 3), (2, 3)]
+    graph = nx.Graph(edges)
+    graph.add_node(4)
+    graph.add_node(5)
+
+    new_graph = nx.Graph(edges)
+    new_graph.add_edge(4, 5)
+
+    #print('lin: ' + str(lin(graph)))
+    #print('closeness: ' + str(closeness(graph)))
+
+    print(eigenvector(graph))
+    print(eigenvector(new_graph))
---
+++ main.py
@@ -0,0 +1,109 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import centralities as ct
+from bisect import insort
+import tests
+import centralities
+
+if __name__ == '__main__':
+
+    # importing graph
+    with open("graph.txt") as file:
+        edges = [tuple(map(int,line.strip().split())) for line in file]
+
+    nodes = []
+    for x, y in edges:
+        if x not in nodes:
+            insort(nodes, x)
+        if y not in nodes:
+            insort(nodes, y)
+
+    # adding isolated nodes
+    isolated = True
+    if isolated:
+        isolated_nodes = [int(item) for item in input("add, if present, isolated nodes (separated by a whitespace) : ").split()]
+        for node in isolated_nodes:
+            if node not in nodes:
+                insort(nodes, node)
+
+    # creating graph
+    temp_graph = nx.empty_graph()
+    temp_graph.add_nodes_from(nodes)
+    temp_graph.add_edges_from(edges)
+
+    # adding new_edge
+    new_edge = tuple(map(int, input('insert new edge: ').split()))
+    if len(new_edge) != 2 or new_edge in temp_graph.edges:
+        exit(1)
+    new_edges = edges
+    new_edges.append(new_edge)
+    new_nodes = nodes
+    if new_edge[0] not in new_nodes:
+        insort(new_nodes, new_edge[0])
+    if new_edge[1] not in new_nodes:
+        insort(new_nodes, new_edge[1])
+
+    print('For matters of consistency, nodes should be labeled from 1 to *node cardinality*, without holes.\nFor this reason, labels will be remapped.')
+
+    # creating new_graph
+    temp_new_graph = nx.empty_graph()
+    temp_new_graph.add_nodes_from(new_nodes)
+    temp_new_graph.add_edges_from(new_edges)
+
+    # mapping
+    mapping = {x: y for y, x in enumerate(temp_graph.nodes, 1)}
+    graph = nx.relabel_nodes(temp_graph, mapping)
+
+    # new_mapping
+    new_mapping = {x: y for y, x in enumerate(temp_new_graph.nodes, 1)}
+    new_graph = nx.relabel_nodes(temp_new_graph, new_mapping)
+
+    # adding edge_colors and showing graph
+    # edge_colors = ['r' if edge == new_edge or edge == (new_edge[1], new_edge[0]) else 'k' for edge in new_graph.edges]
+    # pos = nx.drawing.layout.spring_layout (graph, k=1, seed=200)
+    # nx.draw(new_graph, with_labels=True, edge_color=edge_colors)
+    # plt.show()
+
+    #centralities
+    # centralities = {}
+    # new_centralities = {}
+    #
+    # centralities = ct.degree(graph)
+    # new_centralities = ct.degree(new_graph)
+    #
+    # for k in centralities:
+    #     print(str(k) + ' : ' + str(centralities[k]) + ' , ' + str(new_centralities[k]))
+
+
+    # print(list(nx.all_simple_paths(new_graph, source=1, target=2, cutoff=6)))
+
+    #tests.katz(graph)
+
+    #print(graph.edges)
+    #print(new_graph.edges)
+
+    #print(centralities.eigenvector(graph))
+    #print(centralities.eigenvector(new_graph))
+
+    #print(centralities.seeley(graph))
+    #print(centralities.seeley(new_graph))
+
+    eigen = centralities.eigenvector(graph)
+    new_eigen = centralities.eigenvector(new_graph)
+
+    print(eigen)
+    print(new_eigen)
+    for k in eigen.keys():
+        if new_eigen[k] < eigen[k] and (k == new_edge[0] or k == new_edge[1]):
+            print('EIGENVECTOR --> node ' + str(k))
+            print(eigen)
+            print(new_eigen)
+
+    seeley = centralities.seeley(graph)
+    new_seeley = centralities.seeley(new_graph)
+    for k in seeley.keys():
+        if new_seeley[k] < seeley[k] and (k == new_edge[0] or k == new_edge[1]):
+            print('SEELEY --> node' + str(k))
+            print(seeley)
+            print(new_seeley)
+
